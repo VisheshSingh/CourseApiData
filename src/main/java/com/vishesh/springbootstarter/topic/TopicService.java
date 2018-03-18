d657620d2a46c9bf12d8efd243af3d8c81e036e2
Vishesh <visheshsingh16@gmail.com>
2018-03-17 20:06:16 -0500

    Making crud operations with repository

--- src/main/java/com/vishesh/springbootstarter/topic/TopicService.java
+++ src/main/java/com/vishesh/springbootstarter/topic/TopicService.java
@@ -3,42 +3,43 @@ package com.vishesh.springbootstarter.topic;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
+
+	@Autowired
+	private TopicRepository topicRepository;
+
 	private List<Topic> topics = new ArrayList<>(
 			Arrays.asList(new Topic("spring", "spring framework", "spring framework description"),
 					new Topic("java", "java core", "core java description"),
 					new Topic("javascript", "javascript es6", "javascript description")));
 
 	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 
-	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	public Optional<Topic> getTopic(String id) {
+		return topicRepository.findById(id);
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 
 	}
 
 	public void updateTopic(String id, Topic topic) {
-		for (int i = 0; i < topics.size(); i++) {
-			Topic t = topics.get(i);
-			if (t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
-
+		topicRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 
 	}
 }
